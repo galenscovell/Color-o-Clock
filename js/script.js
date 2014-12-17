e15f6712c92f7baa51707b34e6199c0ce942158f
Galen Scovell <galen.scovell@gmail.com>
2014-12-16 17:06:54 -0800

    Change Name, add HSLA option
    Cool beans.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 
 
-function runClock() {
+function runHSLAClock() {
+  var currentTime = new Date();
+
+  var currentHours = currentTime.getHours();
+  var currentMinutes = currentTime.getMinutes();
+  var currentSeconds = currentTime.getSeconds();
+
+  currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+  currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+  var timeString = currentSeconds + ":" + currentMinutes + ":" + currentHours;
+
+  $('#clock').html(timeString);
+  $('#info').html("hsla(" + (currentSeconds * 6) + ", " + ((currentMinutes * 1.5) + 10) + "%, " + (currentHours) + "%, 1)");
+  $('#main_container').css("background", "hsla(" + (currentSeconds * 3) + ", " + ((currentMinutes * 1.5) + 10) + "%, " + currentHours + "%, 1)");
+  $("#switch span").html("HEX");
+};
+
+
+function runHexClock() {
   var currentTime = new Date();
 
   var currentHours = currentTime.getHours();
@@ -16,12 +36,26 @@ function runClock() {
   var hexValue = "#" + currentHours + currentMinutes + currentSeconds;
 
   $('#clock').html(timeString);
-  $('#hex').html(hexValue);
+  $('#info').html(hexValue)
   $('#main_container').css("background", hexValue);
+  $("#switch span").html("HSLA");
 };
 
 
 $(document).ready(function() {
-  runClock();
-  setInterval('runClock()', 1000);
+  runHSLAClock();
+  intervalHSLA = setInterval('runHSLAClock()', 1000);
+});
+
+
+$("#switch").click(function() {
+  if ($("#switch span").text() == "HSLA") {
+    clearInterval(intervalHex);
+    runHSLAClock();
+    intervalHSLA = setInterval('runHSLAClock()', 1000);
+  } else {
+    clearInterval(intervalHSLA);
+    runHexClock();
+    intervalHex = setInterval('runHexClock()', 1000);
+  }
 });
